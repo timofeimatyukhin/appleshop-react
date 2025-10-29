4731b5b4eb60de23c53fdf879f0db8a4e185208e
Timofey Matyukhin <timtim11111111112@gmail.com>
2025-10-29 23:32:12 +0700

    font and bag button

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,14 +1,57 @@
-import React from "react";
-import styles from './Button.module.css'
+import React, {useState} from "react";
+import styles from "./Button.module.css";
 
-export const Button = ({text, className = '', ...props}) => {
-  const customBtn = styles[className] || '';
+export const Button = ({
+  text,
+  className = "",
+  ...props
+}) => {
+  const [count, setCount] = useState(0);
+  const customBtn = styles[className] || "";
 
-  return ( 
+  const handleAddToBag = () => {
+    setCount(1);
+  };
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    if (count > 1) {
+      setCount(count - 1);
+    } else {
+      setCount(0);
+    }
+  };
+
+  if (count > 0) {
+    return (
+      <div className={styles.counterContainer}>
+        <button
+        onClick={handleDecrement}
+        className={`${styles.counterBtn}`}
+        >-</button>
+        <span className={styles.count}>{count}</span>
+        <button
+        onClick={handleIncrement}
+        className={`${styles.counterBtn}`}
+        >+</button>
+      </div>
+    );
+  }
+
+  return (
     <>
-      <button {...props} className = {`${styles.btn} ${customBtn}`}>{text}</button>
+      <button
+      {...props} 
+      className={`${styles.btn} ${customBtn}`}
+      onClick={handleAddToBag}
+      >
+        {text}
+      </button>
     </>
-  );  
+  );
 };
 
 export default Button;
